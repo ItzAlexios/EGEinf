7985a00fa9db14e3b40a2bf8ccbcb829c2071dd4
Alexios <itzmilk7@gmail.com>
2025-04-15 11:45:45 +0300

    11

--- 25.py
+++ 25.py
@@ -8,43 +8,46 @@ def check(n):
     return 1
 
 for n in range(55_000_001, 60_000_001):
-    for i in range(2, n):
-        if n%i == 0 and check(i) and i%1000 == 777 and i != n:
+    for i in range(2, int(n**0.5)+1):
+        if n%i == 0:
+            if (check(i) and i%1000 == 777 and i != n):
                 print(n, i)
+            elif (check(n//i) and (n//i)%1000 == 777 and i != n):
+                print(n, n//i)
 
-import math
+# import math
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for k in range(2, math.isqrt(n) + 1):
-        if n % k == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for k in range(2, math.isqrt(n) + 1):
+#         if n % k == 0:
+#             return False
+#     return True
 
-def f(limit=55_000_001):
-    results = []
-    i = limit
-    # if limit == 55_000_662:
-    #     print(results)
-    while len(results) < 4:
-        prime_divs = []
-        for j in range(2, math.isqrt(i) + 1):
-            if i % j == 0:
-                if is_prime(j):
-                    prime_divs.append(j)
-                if j != i // j and is_prime(i // j):
-                    prime_divs.append(i // j)
+# def f(limit=55_000_001):
+#     results = []
+#     i = limit
+#     # if limit == 55_000_662:
+#     #     print(results)
+#     while len(results) < 4:
+#         prime_divs = []
+#         for j in range(2, math.isqrt(i) + 1):
+#             if i % j == 0:
+#                 if is_prime(j):
+#                     prime_divs.append(j)
+#                 if j != i // j and is_prime(i // j):
+#                     prime_divs.append(i // j)
         
-        if prime_divs:
+#         if prime_divs:
            
-            prime_divs = sorted(set(prime_divs))
+#             prime_divs = sorted(set(prime_divs))
 
-            for m in prime_divs:
-                if str(m)[-3:] == '777':
-                    results.append((i, m))
-                    print(i, m)
+#             for m in prime_divs:
+#                 if str(m)[-3:] == '777':
+#                     results.append((i, m))
+#                     print(i, m)
                 
-        i += 1
+#         i += 1
 
-f()
+# f()
